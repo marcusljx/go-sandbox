ada3e7916d9a8ae3f40bfe4dc891664009a0e512
marcusljx <marcusljx@gmail.com>
2017-06-04 21:31:03 +0800

    Fix for multi-test reusing same internal F func

--- gest/bind.go
+++ gest/bind.go
@@ -1,15 +1,21 @@
 package gest
 
-import "testing"
+import (
+	"testing"
+)
 
 // bindTests wraps the Tests with the suite's setUpTestFunc and tearDownTestFunc functions
 func (s *Suite) bindTests(tests []testing.InternalTest) (bindedTests []testing.InternalTest) {
 	for _, c := range tests {
-		newC := c // make copy
+		// make copies1
+		newC := c
+		newF := c.F
+
+		// wrap test with setup and teardown
 		newC.F = func(t *testing.T) {
 			s.setUpTestFunc(t)
 			defer s.tearDownTestFunc(t)
-			c.F(t)
+			newF(t)
 		}
 		bindedTests = append(bindedTests, newC)
 	}
@@ -19,11 +25,14 @@ func (s *Suite) bindTests(tests []testing.InternalTest) (bindedTests []testing.I
 // bindBenchmarks wraps the Benchmarks with the suite's setUpBenchmarkFunc and tearDownBenchmarkFunc functions
 func (s *Suite) bindBenchmarks(benchmarks []testing.InternalBenchmark) (bindedBenchmarks []testing.InternalBenchmark) {
 	for _, c := range benchmarks {
-		newC := c // make copy
+		// make copies1
+		newC := c
+		newF := c.F
+
 		newC.F = func(b *testing.B) {
 			s.setUpBenchmarkFunc(b)
 			defer s.tearDownBenchmarkFunc(b)
-			c.F(b)
+			newF(b)
 		}
 		bindedBenchmarks = append(bindedBenchmarks, newC)
 	}
@@ -33,11 +42,14 @@ func (s *Suite) bindBenchmarks(benchmarks []testing.InternalBenchmark) (bindedBe
 // bindExamples wraps the Examples with the suite's setUpExampleFunc and tearDownExampleFunc functions
 func (s *Suite) bindExamples(examples []testing.InternalExample) (bindedExamples []testing.InternalExample) {
 	for _, c := range examples {
-		newC := c // make copy
+		// make copies1
+		newC := c
+		newF := c.F
+
 		newC.F = func() {
 			s.setUpExampleFunc()
 			defer s.tearDownExampleFunc()
-			c.F()
+			newF()
 		}
 		bindedExamples = append(bindedExamples, newC)
 	}
