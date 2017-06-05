9014e866f699f5ea7916c1b1f9db214876ce56b7
Marcus Low Junxiang <marcusljx@gmail.com>
2017-06-05 10:26:25 +0800

    Update sample.go

--- gest/sample.go
+++ gest/sample.go
@@ -1,5 +1,7 @@
 package gest
 
+// Sample is a fake function that does nothing. 
+// It is intended only to serve as an identifier for the ExampleSample() test to use
 func Sample() {
 	// this function does nothing.
 }
