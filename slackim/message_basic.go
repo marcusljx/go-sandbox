124963ca58e49f4dff860d83248569b903bd5958
marcusljx <marcusljx@gmail.com>
2017-06-09 23:30:18 +0800

    Adding channel support

--- slackim/message_basic.go
+++ slackim/message_basic.go
@@ -4,7 +4,8 @@ import "net/http"
 
 // BasicMessage is the top-level message type for attachments
 type BasicMessage struct {
-	Text string `json:"text"`
+	Text    string `json:"text"`
+	Channel string `json:"channel,omitempty"`
 }
 
 // NewBasicMessage returns a new BasicMessage
@@ -14,5 +15,11 @@ func NewBasicMessage(text string) *BasicMessage {
 
 // Send performs the HTTP POST request for the given message
 func (b *BasicMessage) Send(url string) (*http.Response, error) {
+	return postMessage(url, b)
+}
+
+// SendToChannel performs the HTTP POST request for the given message
+func (b *BasicMessage) SendToChannel(url, channel string) (*http.Response, error) {
+	b.Channel = channel
 	return postMessage(url, b)
 }
